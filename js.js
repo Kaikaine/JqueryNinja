e59a0d942039e3986c2d066ea7e4532b76ad984a
Kairi <kairijb@gmail.com>
2018-03-27 21:48:52 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+$('img').click(function(){
+    $(this).hide(500)
+});
+
+$('#btn').click(function(){
+    $('img').fadeIn(500)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
